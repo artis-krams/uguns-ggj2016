5ec94484da6f9a186840cb53f90c1b629153efb2
Janis Skuja <janis.skuja@n-fuse.de>
2016-01-30 00:16:14 +0200

    added reset logic script

---
+++ Assets/Scripts/ResetLogic.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResetLogic : MonoBehaviour {
+
+	private bool mouseEntered;
+
+	void Update () {
+
+		if (mouseEntered == true && Input.GetMouseButtonDown (0)) {
+			//TODO: logic goes here
+		}
+	}
+
+	void OnMouseEnter() {
+		mouseEntered = true;
+	}
+
+	void OnMouseExit() {
+		mouseEntered = false;
+	}
+}
