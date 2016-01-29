6dd4fad25d8489622cc5729732293bdc5e27a907
Janis Skuja <janis.skuja@n-fuse.de>
2016-01-29 22:33:22 +0200

    added timer and burnlogic classes, updated gitignore

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+	public double time;
+
+	public BurnLogic burnLogic;
+	
+	// Update is called once per frame
+	void Update()
+	{
+
+		time -= Time.deltaTime;
+
+		if (time <= 0) {
+			burnLogic.burn();
+		}
+
+	}
+}
