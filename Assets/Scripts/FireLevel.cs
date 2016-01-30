55d83645384834c2d4af093de8e36675b00ec103
aferox <artis.krams@gmail.com>
2016-01-30 13:13:55 +0200

    fire level unfinished

--- Assets/Scripts/FireLevel.cs
+++ Assets/Scripts/FireLevel.cs
@@ -1,16 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class FireLevel : MonoBehaviour {
+public class FireLevel : MonoBehaviour
+{
+    public GameObject[] FireEfects;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        gameObject.transform.localScale = new  Vector3(1,1, LevelManager.level);
+    GameObject currentEffectObject;
+    ParticleSystem currentEffect;
+    int currentIndex;
 
+    void Start()
+    {
+        currentIndex = 0;
+        currentEffectObject = (GameObject)Instantiate(FireEfects[currentIndex],
+            transform.position, Quaternion.identity);
+        currentEffectObject.transform.parent = transform;
+        currentEffect = currentEffectObject.GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentEffectObject.transform.localScale = new Vector3(1, 1, LevelManager.level);
     }
 }
