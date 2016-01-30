8af9c759e6ff02b47284cf433e34846250a0c41d
Janis Skuja <janis.skuja@n-fuse.de>
2016-01-30 12:50:56 +0200

    added delay to reset logic

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -7,4 +7,6 @@ public static class LevelManager
 {
     public static float level = 1f;
 	public static int score = 0;
+	public static int maxBadStuff = 5;
+	public static int badStuffCount = 0;
 }
--- Assets/Scripts/ResetLogic.cs
+++ Assets/Scripts/ResetLogic.cs
@@ -9,14 +9,26 @@ public class ResetLogic : MonoBehaviour
 	private GameObject item;
     private GameObject currentItem;
 	private int randomId;
+	public float waitTime;
+	private bool rutineStarted = false;
 
 	void Update() {
-		if (currentItem == null) {
-			randomId = (int) Random.Range (0, items.Count - 1);
-			item = items.ToArray () [randomId];
-			//Debug.Log ("instantiate object in reset logic");
-			currentItem = (GameObject)Instantiate(item, transform.position, transform.rotation);
-			currentItem.transform.parent = transform;
+		if (currentItem == null && !rutineStarted) {
+			rutineStarted = true;
+			StartCoroutine (InstantiateItem ());
 		}
+	}
+
+	IEnumerator InstantiateItem() {
+
+		yield return new WaitForSeconds (waitTime);
+
+		randomId = (int) Random.Range (0, items.Count - 1);
+		item = items.ToArray () [randomId];
+		//Debug.Log ("instantiate object in reset logic");
+		currentItem = (GameObject)Instantiate(item, transform.position, transform.rotation);
+		currentItem.transform.parent = transform;
+
+		rutineStarted = false;
 	}
 }
